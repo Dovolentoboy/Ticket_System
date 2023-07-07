39a955083a0561bd61694eb48c9771af284dcf4b
Nikita <121382716+Dovolentoboy@users.noreply.github.com>
2023-07-08 00:57:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,24 +10,9 @@ async def on_ready():
     print('Бот готов')
 
 
-@commands.command(name='reload')
-async def reload(ctx,extension):
-    try:
-        bot.unload_extension(f'cogs.{extension}')
-        bot.load_extension(f'cogs.{extension}')
-        await ctx.send(f'cogs.{extension} reloaded')
-    except Exception:
-          await ctx.send(Exception)
-
-    
-
-for filename in os.listdir("./staff"):
-	if filename.endswith(".py"):
-		bot.load_extension(f"staff.{filename[:-3]}")
-
 for filename in os.listdir("./tickets"):
 	if filename.endswith(".py"):
 		bot.load_extension(f"tickets.{filename[:-3]}")         
 
 
-bot.run('MTEyNjk3NjM1MDc1NDk3OTk1Mw.GVB1Fh.llaeTxEpcyzmAUQRuVk54aH70_BPYhPjnEwXYE')
+bot.run('YOUR TOKEN HERE')
